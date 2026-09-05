3a930777692454018b4fbbb3b5944151e46ed6d9
Filip Vateha <vatehafilip@gmail.com>
2026-09-05 08:15:50 +0200

    Remove unused constants and files related to links.
    Deleted `FileConsts` and unused `LinkConsts` entries across projects for cleanup. This reduces redundancy and improves maintainability by removing outdated or unused constants.

--- RawBankEditor/LinkConsts.cs
+++ RawBankEditor/LinkConsts.cs
@@ -2,6 +2,5 @@
 
 public static class LinkConsts
 {
-    public const string LINK_DOWNLOAD = "http://iniss.6f.sk/docs/rawbankeditor/na-stiahnutie/";
     public const string UPDATE = "http://iniss.6f.sk/docs/rawbankeditor/novinky/";
 }
